5dcaf096a1fcf576d1189a8f69dde7bb622f16b1
xljiang <sweetxiaolu@gmail.com>
2016-01-28 22:16:22 -0500

    add Cat class

---
+++ stack_and_queue/Cat.java
@@ -0,0 +1,14 @@
+package stack_and_queue;
+
+// Q3_7
+public class Cat extends Animal{
+	
+	public Cat(String name){
+		super(name);
+	}
+	
+	public String getName(){
+		return "Cat: " + name;
+	}
+
+}
