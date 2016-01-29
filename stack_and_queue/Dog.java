b46b6dac1354c1509ff8618a3e5312245fb54287
xljiang <sweetxiaolu@gmail.com>
2016-01-28 22:16:29 -0500

    add Dog class

---
+++ stack_and_queue/Dog.java
@@ -0,0 +1,14 @@
+package stack_and_queue;
+
+// Q3_7
+public class Dog extends Animal{
+	
+	public Dog(String name){
+		super(name);
+	}
+	
+	public String getName(){
+		return "Dog: " + name;
+	}
+
+}
