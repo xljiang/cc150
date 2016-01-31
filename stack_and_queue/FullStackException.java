cd30a253f76660457013bdabe01231c054f4e9c9
xljiang <sweetxiaolu@gmail.com>
2016-01-30 22:57:02 -0500

    add FullStackException for Q3-1

---
+++ stack_and_queue/FullStackException.java
@@ -0,0 +1,17 @@
+package stack_and_queue;
+
+
+//Q3_1
+public class FullStackException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public FullStackException(){
+		super();
+	}
+	
+	public FullStackException(String msg){
+		super(msg);
+	}
+
+}
