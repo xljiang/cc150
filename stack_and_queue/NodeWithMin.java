26313efa5a399348d1428aaa7279552a1296e5b1
xljiang <sweetxiaolu@gmail.com>
2016-02-01 20:41:25 -0500

    add Q3-2 method1's wrapper

---
+++ stack_and_queue/NodeWithMin.java
@@ -0,0 +1,14 @@
+package stack_and_queue;
+
+
+// Q3-2 method1 wrapper
+public class NodeWithMin {
+
+	public int value;
+	public int min;
+	public NodeWithMin(int v, int m){
+		value = v;
+		min = m;
+	}
+	
+}
